a769af1a24c2d394566efd46a029d51a875d8a44
patycorona <pchdz.mxtobn@gmail.com>
2023-07-10 16:36:54 -0500

    actualización

--- app/src/main/java/com/example/snapshots/data/database/FirebaseActions.kt
+++ app/src/main/java/com/example/snapshots/data/database/FirebaseActions.kt
@@ -1,19 +1,19 @@
 package com.example.snapshots.data.database
 
+import android.content.Context
 import com.example.snapshots.data.model.request.UserRequest
 import com.example.snapshots.data.model.response.UserRegisterResponse
 import com.example.snapshots.domain.model.ConstantGeneral.Companion.CODE
 import com.example.snapshots.domain.model.ConstantGeneral.Companion.ERROR
 import com.example.snapshots.domain.model.ConstantGeneral.Companion.MSG_ERROR
 import com.example.snapshots.domain.model.ConstantGeneral.Companion.MSG_SUCCESS
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Single
 import javax.inject.Inject
 
 
-class FirebaseActions @Inject constructor() {
+class FirebaseActions @Inject constructor(context: Context) {
+    val firebaseActions : FirebaseAuth = context.
     fun userRegisterFirebase(userRequest: UserRequest) : Single<UserRegisterResponse>{
         var userRegisterResponse = UserRegisterResponse()
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(userRequest.email, userRequest.pwd)
